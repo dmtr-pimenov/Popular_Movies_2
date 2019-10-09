84156648869a262b0e1b613ca8c11cb42ffaa233
DMITRYP-LAP\Dmitriy Pimenov <dmitriy.pimenov@cellwize.com>
2019-10-10 00:47:49 +0600

    reviiew fragment

--- app/src/main/java/com/example/android/popularmovies/ui/fragment/ReviewsFragment.java
+++ app/src/main/java/com/example/android/popularmovies/ui/fragment/ReviewsFragment.java
@@ -1,27 +1,34 @@
 package com.example.android.popularmovies.ui.fragment;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.data.model.Resource;
+import com.example.android.popularmovies.data.model.Review;
+import com.example.android.popularmovies.databinding.FragmentReviewsBinding;
 import com.example.android.popularmovies.ui.MovieDetailViewModel;
+import com.example.android.popularmovies.ui.adapter.ReviewListAdapter;
+
+import java.util.List;
 
 public class ReviewsFragment extends Fragment {
 
     private MovieDetailViewModel mViewModel;
+    private FragmentReviewsBinding mBinding;
 
     public ReviewsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     */
     public static ReviewsFragment newInstance() {
         ReviewsFragment fragment = new ReviewsFragment();
         fragment.setArguments(new Bundle());
@@ -31,13 +38,60 @@ public class ReviewsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        mViewModel = ViewModelProviders.of(getActivity()).get(MovieDetailViewModel.class);
+        mViewModel = ViewModelProviders.of(getActivity()).get(MovieDetailViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reviews, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_reviews, container, false);
+        mViewModel.getReviewCollection().observe(this, new Observer<Resource<List<Review>>>() {
+            @Override
+            public void onChanged(@Nullable Resource<List<Review>> listResource) {
+                if (listResource.status == Resource.Status.SUCCESS) {
+                    List<Review> data = listResource.data;
+                    if (data.size() == 0) {
+                        showNaMessage();
+                    } else {
+                        setupRecyclerView(data);
+                    }
+
+                } else {
+                    showError();
+                }
+            }
+        });
+        return mBinding.getRoot();
     }
+
+    private void setupRecyclerView(List<Review> data) {
+
+        mBinding.textReviewError.setVisibility(View.GONE);
+        mBinding.textReviewNa.setVisibility(View.GONE);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),
+                LinearLayoutManager.VERTICAL, false);
+        mBinding.recyclerViewReview.setLayoutManager(layoutManager);
+        ReviewListAdapter adapter = new ReviewListAdapter(getContext(), data);
+        mBinding.recyclerViewReview.setAdapter(adapter);
+/*
+        mBinding.recyclerViewTrailers
+                .addItemDecoration(new MarginItemDecorator((int) getContext().getResources().getDimension(R.dimen.double_margin)));
+*/
+        mBinding.recyclerViewReview.setVisibility(View.VISIBLE);
+    }
+
+    private void showNaMessage() {
+        mBinding.textReviewError.setVisibility(View.GONE);
+        mBinding.textReviewNa.setVisibility(View.VISIBLE);
+        mBinding.recyclerViewReview.setVisibility(View.GONE);
+    }
+
+    private void showError() {
+        mBinding.textReviewError.setVisibility(View.VISIBLE);
+        mBinding.textReviewNa.setVisibility(View.GONE);
+        mBinding.recyclerViewReview.setVisibility(View.GONE);
+    }
+
 }
